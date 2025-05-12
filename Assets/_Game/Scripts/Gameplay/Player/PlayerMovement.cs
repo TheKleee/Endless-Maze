98dec1110cd0bb0393ace65dfa607746a31312aa
Stefan Mišić <stefanmisic002@gmail.com>
2025-05-12 13:11:35 +0200

    Krteanje karaktera
    Ubacio sam kretanje karaktera i smanjio sam velicinu lavirinta na 11 da bi bilo laksa igra u trajanju do 2 minuta odp.

--- Assets/_Game/Scripts/Gameplay/Player/PlayerMovement.cs
+++ Assets/_Game/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -1,9 +1,13 @@
-using System.Collections.Generic;
 using UnityEngine;
-using MEC;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement:")]
+    [SerializeField] Transform R;
+    [SerializeField] Transform L;
+    [SerializeField] Transform F;
+    [SerializeField] Transform B;
+
     [Header("Camera:"), SerializeField]
     Camera cam;
 
@@ -15,13 +19,16 @@ public class PlayerMovement : MonoBehaviour
     bool startMoving;
     [SerializeField, Range(0.2f, 5.0f)] float startSpeed = 1f;
     [SerializeField] float maxSpeed = 15.0f;
+
+    [Header("Speed:"), SerializeField]
     float curSpeed = .0f;
-    
+
     [Header("Hand:"), SerializeField]
     Transform pickUpHand;
 
     void Awake()
     {
+        curSpeed = startSpeed;
         rb = GetComponent<Rigidbody>();
         cam = cam == null ? GetComponentInChildren<Camera>() : cam;
         anim = anim == null ? GetComponentInChildren<Animator>() : anim;
@@ -43,7 +50,45 @@ public class PlayerMovement : MonoBehaviour
     #region Movement
     void Move()
     {
+        if (Input.GetAxis("Vertical") == 0)
+        {
+            curSpeed = startSpeed;
+        }
 
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(F.localPosition * curSpeed * Time.fixedDeltaTime);
+
+            if (curSpeed < maxSpeed)
+            {
+                curSpeed += 0.1f;
+            }
+            else
+            {
+                curSpeed = maxSpeed;
+            }
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.Translate(B.localPosition * startSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            var lookPos = R.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, startSpeed * Time.deltaTime);
+        }
+
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            var lookPos = L.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, startSpeed * Time.deltaTime);
+        }
     }
     #endregion movement />
 
